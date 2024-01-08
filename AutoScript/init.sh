a03e8883b5219f6074a920cb688fb1718c66269d
almirus <abrarov.almir@gmail.com>
2024-01-08 17:25:01 +0300

    добавлено выключение автозагрузки прошивки

--- AutoScript/init.sh
+++ AutoScript/init.sh
@@ -2,12 +2,11 @@
 export PATH="$PATH:/usr/local/bin:/usr/net/vudu:/usr/net/browser/bin"
 export LD_LIBRARY_PATH="/usr/local/lib:/usr/net/vudu:/usr/net/browser/lib:/mnt/ubi_boot/forusb/bluetooth/usr/lib:/lib/directfb-1.2-0/systems:/usr/lib/ppc"
 
-# Enable root login without password
-#/bin/echo root::0:0:root,,,:/root:/bin/sh > /etc/passwd
+# Enable root login without password and disable cloud update
+cp -rf /mnt/sda1/AutoScript/var/AutoScript.TSS /var/AutoScript.TSS
 
+# Enable SSH
 /usr/local/sbin/sshd -p 22 &
-
-dmesg > /mnt/sda1/dmesg.log
 
 # Commit filesystem caches to disk
 sync
